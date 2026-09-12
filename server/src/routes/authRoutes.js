e2100b49d3c2d5bbf17f56673f42a44bb6708e2f
Jenish j <jenishjenish638082@gmail.com>
2026-09-12 20:34:21 +0530

    Fix production authentication cookie

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -549,22 +549,23 @@ router.post('/login', async (req, res) => {
     */
 
     res.cookie(
-      'lumora_token',
-      token,
-      {
-        httpOnly: true,
-        secure:
-          process.env.NODE_ENV ===
-          'production',
-        sameSite: 'lax',
-        maxAge:
-          7 *
-          24 *
-          60 *
-          60 *
-          1000
-      }
-    );
+  'lumora_token',
+  token,
+  {
+    httpOnly: true,
+
+    secure: true,
+
+    sameSite: 'none',
+
+    maxAge:
+      7 *
+      24 *
+      60 *
+      60 *
+      1000
+  }
+);
 
 
     /*
